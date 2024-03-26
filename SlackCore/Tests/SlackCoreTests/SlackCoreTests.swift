a76afcfbef9a8d6cb1e04542e62fdc7a641cb790
Jinwoo Kim <kidjinwoo@me.com>
2024-03-27 02:45:33 +0900

    no message

--- SlackCore/Tests/SlackCoreTests/SlackCoreTests.swift
+++ SlackCore/Tests/SlackCoreTests/SlackCoreTests.swift
@@ -11,11 +11,46 @@ import XCTest
 
 final class SlackCoreTests: XCTestCase {
     func testAll() async {
+        let eventRecorder = Event.ConsoleOutputRecorder(options: []) { string in
+            print(string)
+        }
+        
         var configuration: Testing.Configuration = .init()
+        
         configuration.testFilter = Testing.Configuration.TestFilter(includingAnyOf: [.init(kind: .stringLiteral("testGetUserProfileDictionary"))])
+        
+        configuration.isParallelizationEnabled = false
+        configuration.eventHandler = { event, context in
+            eventRecorder.record(event, in: context)
+            
+            guard case let .issueRecorded(issue) = event.kind else {
+                return
+            }
+            if !issue.isKnown {
+                self.record(
+                    XCTIssue(
+                        type: .assertionFailure,
+                        compactDescription: String(describing: issue),
+                        sourceCodeContext: .init(issue.sourceContext)
+                    )
+                )
+            }
+        }
+        
         let runner: Testing.Runner = await .init(configuration: configuration)
         await runner.run()
-        
-//        await XCTestScaffold.runAllTests(hostedBy: self)
     }
+}
+
+extension XCTSourceCodeContext {
+  fileprivate convenience init(_ sourceContext: SourceContext) {
+    let addresses = sourceContext.backtrace?.addresses.map { $0 as NSNumber } ?? []
+    let sourceLocation = sourceContext.sourceLocation.map { sourceLocation in
+      XCTSourceCodeLocation(
+        filePath: sourceLocation._filePath,
+        lineNumber: sourceLocation.line
+      )
+    }
+    self.init(callStackAddresses: addresses, location: sourceLocation)
+  }
 }
